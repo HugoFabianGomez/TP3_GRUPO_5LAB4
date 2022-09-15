3994f65345768bd0fae43de2ae538d901e85c623
pabloalderete <pablo.alderete@live.com>
2022-09-14 23:26:45 -0300

    IOException DniInvalido y clase Principal

---
+++ src/ejercicio1/DniInvalido.java
@@ -0,0 +1,17 @@
+package ejercicio1;
+import java.io.IOException;
+
+public class DniInvalido extends IOException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DniInvalido(){
+		
+	}
+
+	@Override
+	public String getMessage() {
+		return "Error - El DNI solo puede contener ";
+	}
+	
+}
